e5c158b7c45e37dcf79bf40d7557acde2748f4a5
vcassotti <vcassotti@gmail.com>
2019-05-13 22:28:08 +0200

    add spring boot bootstrap

---
+++ src/main/java/org/vow/actos/ActosApplication.java
@@ -0,0 +1,13 @@
+package org.vow.actos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActosApplication.class, args);
+	}
+
+}
